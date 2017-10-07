bc85b2972e74c22e1a0d7df9637b560ddf747c04
Lycheejam <32588863+Lycheejam@users.noreply.github.com>
2017-10-07 16:18:43 +0900

    Add files via upload

---
+++ drugdrop_test/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace drugdrop_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.filePathBox = new System.Windows.Forms.TextBox();
+            this.resetBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(12, 42);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(260, 207);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb1.TabIndex = 0;
+            this.pb1.TabStop = false;
+            // 
+            // filePathBox
+            // 
+            this.filePathBox.AllowDrop = true;
+            this.filePathBox.Location = new System.Drawing.Point(13, 13);
+            this.filePathBox.Name = "filePathBox";
+            this.filePathBox.Size = new System.Drawing.Size(180, 19);
+            this.filePathBox.TabIndex = 1;
+            this.filePathBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.filePathBox_DragDrop);
+            this.filePathBox.DragEnter += new System.Windows.Forms.DragEventHandler(this.filePathBox_DragEnter);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(200, 13);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(72, 23);
+            this.resetBtn.TabIndex = 2;
+            this.resetBtn.Text = "リセット";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.filePathBox);
+            this.Controls.Add(this.pb1);
+            this.Name = "Form1";
+            this.Text = "DragAndDrop_test";
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.TextBox filePathBox;
+        private System.Windows.Forms.Button resetBtn;
+    }
+}
+
---
+++ drugdrop_test/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace drugdrop_test
+{
+    public partial class Form1 : Form
+    {
+        /*
+         * drag&dropでファイルパスを取得して画像を表示するプログラム
+         * 参考 : Drag&Dropされたファイルのファイル名を取得する: .NET Tips: C#, VB.NET
+         * https://dobon.net/vb/dotnet/control/droppedfile.html
+         */
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void filePathBox_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] fileName = (string[]) e.Data.GetData(DataFormats.FileDrop, false);
+            filePathBox.Text = fileName[0];
+
+            pb1.Image = Image.FromFile(@fileName[0]);
+        }
+
+        //コントロール内にドラッグされた時に実行される。
+        // 参考 : Control.DragEnter イベント (System.Windows.Forms)
+        // https://msdn.microsoft.com/ja-jp/library/system.windows.forms.control.dragenter(v=vs.90).aspx
+        private void filePathBox_DragEnter(object sender, DragEventArgs e)
+        {
+            //ファイルかどうかを判断する。
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                //ファイルならばコピー
+                e.Effect = DragDropEffects.Copy;
+            }else{
+                //ファイルでなければ何もしない。
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            //picturBox,textBox初期化
+            pb1.Image = null;
+            filePathBox.Text = "";
+
+            //fileName配列は呼び出し時に宣言されるので初期化不要？
+        }
+    }
+}
